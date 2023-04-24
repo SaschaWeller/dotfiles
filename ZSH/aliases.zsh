7a4e505f946c521ac68175fd0f3640ed720eabf5
Sascha Weller <46193753+SaschaWeller@users.noreply.github.com>
2023-04-24 14:51:27 +0200

    Add files for ZSH

---
+++ ZSH/aliases.zsh
@@ -0,0 +1,10 @@
+# Alias
+# ---
+#
+
+
+# ALIAS COMMANDS
+alias cls="clear"
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias grep='grep --color'
